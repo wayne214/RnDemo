d74937ee64cfc7f9cc96ee489867ebce860e591f
wayne <jun_wei_like1@163.com>
2019-09-26 17:09:43 +0800

    animated

--- src/pages/discussList.js
+++ src/pages/discussList.js
@@ -10,19 +10,76 @@ import {
     AlertIOS,
     Animated,
     RefreshControl,
-    TouchableOpacity
+    TouchableOpacity,
+    Easing
 } from 'react-native'
 let {width, height} = Dimensions.get('window')
 
 import DiscussItem from './../components/discuss/discuss'
 
 class discussList extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            fadeInOpacity: new Animated.Value(0), // 初始值
+            rotation:new Animated.Value(0),
+            fontSize: new Animated.Value(0)
+        }
+    }
+
+    componentDidMount() {
+        // Animated.timing(this.state.fadeInOpacity,{
+        //     toValue: 1,
+        //     duration: 10000,
+        //     easing: Easing.linear
+        // }).start()
+        let timing = Animated.timing;
+        Animated.parallel(['fadeInOpacity','rotation','fontSize'].map(
+            property => {
+                return timing(this.state[property],{
+                    toValue: 1,
+                    duration: 10000,
+                    easing: Easing.linear
+                })
+            }
+        )).start()
+
+    }
 
     render() {
         return(
-            <DiscussItem/>
+            <View style={{flex: 1}}>
+                <DiscussItem/>
+                <Animated.View
+                    style={[
+                        styles.demo,
+                        {opacity: this.state.fadeInOpacity,
+                            transform:[{
+                            rotateZ: this.state.rotation.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: ['0deg', '360deg']
+                            })
+                            }]
+                        }
+                    ]}
+                >
+                    <Animated.Text style={{fontSize: this.state.fontSize.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [12, 26]
+                        }),
+                        color: 'red'}}>
+                        悄悄的，我出现了😈💨</Animated.Text>
+                </Animated.View>
+            </View>
+
         )
     }
 }
+
+const styles = StyleSheet.create({
+    demo: {
+        backgroundColor: '#fff'
+    }
+})
 
 export default discussList

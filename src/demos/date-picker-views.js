78193147a89891a252c1104eca67659497c9da9c
wayne <jun_wei_like1@163.com>
2020-03-31 16:17:23 +0800

    时间

--- src/demos/date-picker-views.js
+++ src/demos/date-picker-views.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import {View, Text, TouchableOpacity} from 'react-native';
 import Picker from 'react-native-picker'
 
+import {isToday} from '../util'
+
 class DatePickerViews extends Component{
 
     componentDidMount(): void {
@@ -66,8 +68,67 @@ class DatePickerViews extends Component{
                 minutes.push(i);
             }
         }
-        let pickerData = [years, months, days, ['上午', '下午'], hours, minutes];
+
         let date = new Date();
+        let year = date.getFullYear()
+        let monthAndDay = []
+
+        var weekArray = new Array("周日", "周一", "周二", "周三", "周四", "周五", "周六");
+        for(let i= 1; i<13; i++){
+            if(i === 2) {
+                if(year%4 === 0 && ((year % 100) !== 0) || ((year % 400) === 0)){
+                    for(let j=1;j<30; j++){
+                        let da = `2020-${i}-${j}`
+                        var week = weekArray[new Date(da).getDay()]
+                        let obj = i + '月' + j + '日' +week
+                        if(isToday(da)){
+                            monthAndDay.push('今天')
+                        } else {
+                            monthAndDay.push(obj);
+                        }
+                    }
+                }else {
+                    for(let j=1;j<29; j++){
+                        let da = `2020-${i}-${j}`
+                        var week = weekArray[new Date(da).getDay()]
+                        let obj = i + '月' + j + '日'+ week
+                        if(isToday(da)){
+                            monthAndDay.push('今天')
+                        } else {
+                            monthAndDay.push(obj);
+                        }
+                    }
+                }
+            } else {
+                if (i in {4:1, 6:1, 9:1, 11:1}) {
+                    for(let j=1;j<31; j++){
+                        let da = `2020-${i}-${j}`
+                        var week = weekArray[new Date(da).getDay()]
+                        let obj = i + '月' + j + '日'+week
+                        if(isToday(da)){
+                            monthAndDay.push('今天')
+                        } else {
+                            monthAndDay.push(obj);
+                        }
+                    }
+                } else {
+                    for(let j=1;j<32; j++){
+                        let da = `2020-${i}-${j}`
+                        var week = weekArray[new Date(da).getDay()]
+                        let obj = i + '月' + j + '日'+week
+                        if(isToday(da)){
+                            monthAndDay.push('今天')
+                        } else {
+                            monthAndDay.push(obj);
+                        }
+                    }
+                }
+            }
+        }
+
+        console.log('---dddd', monthAndDay, date.getFullYear());
+
+        let pickerData = [years, months, days, ['上午', '下午'], hours, minutes];
         let selectedValue = [
             date.getFullYear(),
             date.getMonth()+1,
--- src/util/index.js
+++ src/util/index.js
@@ -10,3 +10,18 @@ const basePx = 375
 export default function px2dp(px) {
     return px *  deviceW / basePx
 }
+
+export const isToday = (str) => {
+    if (new Date(str).toDateString() === new Date().toDateString()) {
+        //今天
+        console.log("当天");
+
+        return true
+    } else if (new Date(str) < new Date()){
+        //之前
+        console.log("以前的日期");
+
+        return false
+    }
+
+}
